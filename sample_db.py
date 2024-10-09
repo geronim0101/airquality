48e95abc2500f6c060ee0fe9bc9fd7cc33514d14
kjvmartinez <user@Users-MacBook-Air.local>
2024-10-09 14:23:40 +0800

    add function for read_dht11

--- sample_db.py
+++ sample_db.py
@@ -1,11 +1,21 @@
 import sqlite3
 
 
+def read_dht11():
+    humidity, temperature = 11, 10
+    return humidity, temperature
+
+
 def get_sensor_data():
     # Simulate reading sensor data (replace with actual sensor readings)
+    h, t = read_dht11()
+
     sensor_data = {
         'pm2': 2.0,
-        'pm10': 4.5
+        'pm10': 4.5,
+        'humidity': h,
+        'temperature': t
+
     }
     return sensor_data
 
@@ -17,8 +27,8 @@ def insert_sensor_data(sensor_data):
 
     # Insert the air quality data into the database using the dictionary
     cursor.execute('''
-    INSERT INTO read_data (created_at, pm2, pm10)
-    VALUES (CURRENT_TIMESTAMP, :pm2, :pm10)
+    INSERT INTO read_data (created_at, pm2, pm10, humidity, temperature)
+    VALUES (CURRENT_TIMESTAMP, :pm2, :pm10, :humidity, :temperature)
     ''', sensor_data)
 
     # Commit and close the connection
